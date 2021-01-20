a40f22d44a0d1545a18fb376908040321be02ae9
Darryl Yeo <dev@darryl-yeo.com>
2021-01-20 00:15:50 -0800

    Implement CreatorSummary and home page section

---
+++ web/src/components/CreatorSummary.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let name: string
+	export let summary: string
+	export let image: string | URL
+
+	export let incomingRate = 0
+	export let incomingToken = 'ETH'
+	export let subscriberCount = 0
+</script>
+
+<style>
+	.creator-summary {
+		--space-inner: 0.75em;
+		justify-items: center;
+		text-align: center;
+	}
+
+	img {
+		border-radius: 50%;
+	}
+
+	.summary {
+		color: var(--accent-color);
+		font-weight: normal;
+	}
+
+	.card.style-indented {
+		background: linear-gradient(#E791F522, #9C82E522);
+		box-shadow: inset #E791F544 0 2px 4px;
+		--space-outer: 1.5rem;
+	}
+</style>
+
+<article class="creator-summary card">
+	<img src={image.toString()} alt="name" width="100" />
+	<h3>{name}</h3>
+	<h4 class="summary">{summary}</h4>
+	<div class="row">
+		<div class="card style-indented">
+			<span><strong>{incomingRate} {incomingToken}</strong>/month</span>
+		</div>
+		<div class="card style-indented">
+			{subscriberCount} subscriber{subscriberCount === 1 ? '' : 's'}
+		</div>
+	</div>
+</article>
--- web/src/pages/home.svelte
+++ web/src/pages/home.svelte
@@ -1,20 +1,61 @@
 <script lang="ts">
+	const creators = [{
+		name: 'money.eth',
+		summary: 'DeFi Builder, Curve.fi',
+		image: 'https://picsum.photos/200/200',
+	},{
+		name: '0x15A3FD40...',
+		summary: 'NFT Creator at Aavegotchi',
+		image: 'https://picsum.photos/200/200?1',
+	},{
+		name: 'money.eth',
+		summary: 'DeFi Builder, Curve.fi',
+		image: 'https://picsum.photos/200/200?2',
+	}]
+
 	import Button from '../components/Button.svelte'
+	import CreatorSummary from '../components/CreatorSummary.svelte'
 </script>
 
 <style>
 	section {
-		font-size: 2em;
+		font-size: 1.5em;
 		padding: 4rem;
 		/* justify-items: center; */
 		/* grid-template-columns: minmax(auto, 120rem); */
 		--space-inner: 2rem;
+		min-height: 70vh;
+		align-content: center;
+	}
+
+	section.accented {
+		background: radial-gradient(var(--accent-color-light) 50%, var(--accent-color));
+	}
+	section.accented h2 {
+		color: white;
+	}
+
+	.creators {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(16em, 1fr));
+		font-size: 0.8em;
+		gap: var(--space-inner);
 	}
 </style>
 
-<section>
-	<h2>Make every second count.</h2>
+<section id="top">
+	<h2>Reward creativity.<br>Make every second count.</h2>
 	<div>
-		<Button href="explore">Get started</Button>
+		<Button href="explore">Start accepting donations</Button>
 	</div>
 </section>
+
+<section id="creators" class="accented">
+	<h2>We love our developers and content creators</h2>
+	<div class="creators">
+		{#each creators as creatorInfo}
+			<CreatorSummary {...creatorInfo} />
+		{/each}
+	</div>
+</section>
+
