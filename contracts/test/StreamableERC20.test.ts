d3724b4aaeddb00f995eabd06d03454359674ce8
blag0j <dimovski.blagoj@students.finki.ukim.mk>
2021-01-23 00:05:02 +0100

    test setup update

--- contracts/test/StreamableERC20.test.ts
+++ contracts/test/StreamableERC20.test.ts
@@ -19,10 +19,10 @@ describe('StreamableERC20', function () {
 	beforeEach(async function () {
 		/*const StreamableERC20 = await ethers.getContractFactory('IStreamableERC20')
 		const contract = await StreamableERC20.deploy()*/
-		this.contract = await ethers.getContractAt('IStreamableERC20', '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7')
+		this.contract = await ethers.getContractAt('strETH', '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0')
 	})
 
-	function amount (this: any, n: string) {
+	function amount(this: any, n: string) {
 		return ethers.utils.parseEther(n) //TODO: take decimals into account
 	}
 
@@ -32,48 +32,48 @@ describe('StreamableERC20', function () {
 
 	it('should have 0 balance by default', async function () {
 		const balance = await this.contract.balanceOf(this.accounts[0].address)
-		expect(balance).to.be.bignumber.eq(ethers.utils.parseEther('0'))
+		balance.should.be.bignumber.eq(ethers.utils.parseEther('0'))
 	})
 
-	describe('with inital token balance', function () {
-
-		beforeEach(async function () {
-			//TODO: mint tokens for user (this.accounts[0].address)
-		})
-
-		async function subscribe(this: any) {
-			// subscribe from [0] to [1] with 1 ETH / block, maximum of 10 ETH
-			await this.contract.subscribe(this.accounts[0].address, this.accounts[1].address, amount('1'), amount('10'))
-		}
-
-		async function expectSubscription(this: any, from: string, to: string, rate: BigNumber, maxAmount: BigNumber) {
-			const [_rate, _maxAmount] = this.contract.getSubscription(from, this.accounts[1].address)
-			expect(_rate).to.be.bignumber.eq(rate)
-			expect(_maxAmount).to.be.bignumber.eq(amount('10'))
-		}
-
-		it('should subscribe successfully', async function () {
-			await subscribe()
-			expectSubscription(this.accounts[0].address, this.accounts[1].address, amount('1'), amount('10'))
-		})
-
-		it('should unsubscribe successfully', async function () {
-			await subscribe()
-			await this.contract.subscribe(this.accounts[0].address, this.accounts[1].address, 0, 0)
-			expectSubscription(this.accounts[0].address, this.accounts[1].address, amount('0'), amount('0'))		
-		})
-
-		it('should update balances correctly', async function () {
-			await subscribe()
-			
-			await mineBlocks(1)
-
-			const balance0 = await this.contract.balanceOf(this.accounts[0].address)
-			expect(balance0).to.be.bignumber.eq(amount('9'))
-
-			const balance1 = await this.contract.balanceOf(this.accounts[1].address)
-			expect(balance1).to.be.bignumber.eq(amount('1'))
-		}
-
-	})
+	// describe('with inital token balance', function () {
+	//
+	// 	beforeEach(async function () {
+	// 		//TODO: mint tokens for user (this.accounts[0].address)
+	// 	})
+	//
+	// 	async function subscribe(this: any) {
+	// 		// subscribe from [0] to [1] with 1 ETH / block, maximum of 10 ETH
+	// 		await this.contract.subscribe(this.accounts[0].address, this.accounts[1].address, amount('1'), amount('10'))
+	// 	}
+	//
+	// 	async function expectSubscription(this: any, from: string, to: string, rate: BigNumber, maxAmount: BigNumber) {
+	// 		const [_rate, _maxAmount] = this.contract.getSubscription(from, this.accounts[1].address)
+	// 		expect(_rate).to.be.bignumber.eq(rate)
+	// 		expect(_maxAmount).to.be.bignumber.eq(amount('10'))
+	// 	}
+	//
+	// 	it('should subscribe successfully', async function () {
+	// 		await subscribe()
+	// 		expectSubscription(this.accounts[0].address, this.accounts[1].address, amount('1'), amount('10'))
+	// 	})
+	//
+	// 	it('should unsubscribe successfully', async function () {
+	// 		await subscribe()
+	// 		await this.contract.subscribe(this.accounts[0].address, this.accounts[1].address, 0, 0)
+	// 		expectSubscription(this.accounts[0].address, this.accounts[1].address, amount('0'), amount('0'))
+	// 	})
+	//
+	// 	it('should update balances correctly', async function () {
+	// 		await subscribe()
+	//
+	// 		await mineBlocks(1)
+	//
+	// 		const balance0 = await this.contract.balanceOf(this.accounts[0].address)
+	// 		expect(balance0).to.be.bignumber.eq(amount('9'))
+	//
+	// 		const balance1 = await this.contract.balanceOf(this.accounts[1].address)
+	// 		expect(balance1).to.be.bignumber.eq(amount('1'))
+	// 	})
+	//
+	// })
 })
--- contracts/test/chai-setup.ts
+++ contracts/test/chai-setup.ts
@@ -1,5 +1,5 @@
 import chaiModule from 'chai'
 import {chaiEthers} from 'chai-ethers'
 chaiModule.use(chaiEthers)
-chaiModule.use(require('chai-bignumber'))
+chaiModule.use(require('chai-bignumber')())
 export = chaiModule
