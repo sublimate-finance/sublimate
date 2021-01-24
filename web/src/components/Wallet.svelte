235d4133e990e03b3f0dd50dc1c81cc4a0236f3f
ulydev <ulydev@live.fr>
2021-01-24 06:14:21 +0100

    tweak wallet display to reflect mockup

--- web/src/components/Wallet.svelte
+++ web/src/components/Wallet.svelte
@@ -17,14 +17,23 @@
 	}
 </style>
 
-<Button on:click={() => modalIsOpen = !modalIsOpen}>
-	{#if $wallet.address}
-		<Blockie address={$wallet.address} />
-		<Address address={$wallet.address} format="middle-truncated" linked={false} />
-	{:else}
+{#if $wallet.address}
+	<button class="flex flex-row items-center space-x-3 px-2 py-1 pl-4 bg-purple-200 rounded-md border-none shadow-inner" on:click={() => modalIsOpen = !modalIsOpen}>
+		<span class="">
+			12.765 ETH <!-- FIXME: text is not misaligned vertically due missing css reset -->
+		</span>
+		<div class="flex flex-row items-center rounded-md bg-white pl-3 pr-1 py-1 space-x-1 font-semibold">
+			<Address address={$wallet.address} format="middle-truncated" linked={false} />
+			<div class="rounded-full overflow-hidden w-4 h-4">
+				<Blockie address={$wallet.address} />
+			</div>
+		</div>
+	</button>
+{:else}
+	<Button on:click={() => modalIsOpen = !modalIsOpen}>
 		Connect Wallet
-	{/if}
-</Button>
+	</Button>
+{/if}
 
 {#if modalIsOpen}
 	<Modal on:close={() => modalIsOpen = false}>
