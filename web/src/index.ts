b8ec3032aae39ebfdf575d2f2429254f45c757f4
Darryl Yeo <dev@darryl-yeo.com>
2021-01-25 18:35:18 -0800

    Move CSS from HTML template to module imports

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,6 +2,8 @@ import {hookup} from 'named-logs-console'
 import './service-worker-handler'
 import App from './App.svelte'
 import 'tailwindcss/tailwind.css'
+import '../public/global.css'
+import '../public/fonts/fonts.css'
 
 hookup()
 
