8897a6bd8f8dd1a032002c5eef61f852cccc2f2f
Darryl Yeo <dev@darryl-yeo.com>
2021-01-25 22:49:26 -0800

    Add table of mock subscription info to creator summary

--- web/src/components/CreatorSummary.svelte
+++ web/src/components/CreatorSummary.svelte
@@ -6,6 +6,48 @@
 	export let incomingRate = 0
 	export let incomingToken = 'ETH'
 	export let subscriberCount = 0
+
+	export let subscriptionData = [{
+		asset: 'ETH',
+		incomingRate: 100,
+		subscriberCount: 10,
+	}, {
+		asset: 'DAI',
+		incomingRate: 10,
+		subscriberCount: 5,
+	}]
+
+
+	export let timeInterval: 'block' | 'day' | 'month' | 'year' = 'day'
+	export let baseCurrency = 'ETH'
+
+	$: prices = {
+		'ETH': 2000 * 1e-18,
+		'DAI': 1 * 1e-18
+	}
+	$: averageBlocksPerTimeInterval = {
+		'year': 1000,
+		'month': 1000 / 12,
+		'day': 1000 / 365.25,
+		'block': 1
+	}
+	function convertTokenRate(token, tokensPerBlock, timeInterval, baseCurrency){
+		const amount = tokensPerBlock * averageBlocksPerTimeInterval[timeInterval] * prices[baseCurrency]/prices[token]
+		return `${amount.toFixed(3)} ${baseCurrency}/${timeInterval}`
+	}
+
+	$: tableData = subscriptionData.map(row => {
+		const value = row.asset
+		return {
+			'Asset': row.asset,
+			'Earning': convertTokenRate(row.asset, row.incomingRate, timeInterval, baseCurrency),
+			'Subscribers': row.subscriberCount
+		}
+	})
+
+
+	import Table from './Table.svelte'
+	import TokenName from './TokenName.svelte'
 </script>
 
 <style>
@@ -42,4 +84,13 @@
 			{subscriberCount} subscriber{subscriberCount === 1 ? '' : 's'}
 		</div>
 	</div>
+	<Table data={tableData}>
+		<div slot="cell" let:key let:value>
+			{#if key === 'Asset'}
+				<TokenName token={value} />
+			{:else}
+				{value}
+			{/if}
+		</div>
+	</Table>
 </article>
--- web/src/pages/home.svelte
+++ web/src/pages/home.svelte
@@ -3,11 +3,20 @@
 		name: 'money.eth',
 		summary: 'DeFi Builder, Curve.fi',
 		image: 'https://picsum.photos/200/200',
-	},{
+		subscriptionData: [{
+			asset: 'ETH',
+			incomingRate: 100,
+			subscriberCount: 10,
+		}, {
+			asset: 'DAI',
+			incomingRate: 10,
+			subscriberCount: 5,
+		}]
+	}, {
 		name: '0x15A3FD40...',
 		summary: 'NFT Creator at Aavegotchi',
 		image: 'https://picsum.photos/200/200?1',
-	},{
+	}, {
 		name: 'money.eth',
 		summary: 'DeFi Builder, Curve.fi',
 		image: 'https://picsum.photos/200/200?2',
