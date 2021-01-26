257a543a4a88d67905074d6fa1998081fb5d6b64
Darryl Yeo <dev@darryl-yeo.com>
2021-01-25 18:36:06 -0800

    Split <Nav> into <Header>, <Logo>, and <Nav>

---
+++ web/src/components/Logo.svelte
@@ -0,0 +1,16 @@
+<style>
+	h1 {
+		visibility: hidden;
+	}
+
+	img {
+		width: auto;
+		height: 100%;
+		object-fit: contain;
+	}
+</style>
+
+<div class="logo stack">
+	<img src="/images/sublimate.svg" alt="Sublimate" />
+	<h1>Sublimate</h1>
+</div>
--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -4,35 +4,12 @@
 	export let links: LinkInfo[]
 
 	import NavLink from './NavLink.svelte'
-	import Wallet from './Wallet.svelte'
 </script>
 
 <style>
 	.nav {
 		display: flex;
 		align-items: stretch;
-		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-
-		padding: 0 var(--space-outer);
-		height: 4.5rem;
-
-		position: sticky;
-		top: 0;
-
-		--space-inner: 2.5rem;
-
-		/* backdrop-filter: var(--overlay-backdrop-filter); */
-		background-color: rgba(255, 255, 255, 0.3);
-		z-index: 1;
-	}
-	.nav:before {
-		content: '';
-
-		position: absolute;
-		inset: 0;
-		z-index: -1;
-
-		backdrop-filter: var(--overlay-backdrop-filter);
 	}
 
 	li {
@@ -64,21 +41,9 @@
 	li > :global(a.active) {
 		border-bottom-color: var(--accent-color);
 	}
-
-	h1 {
-		visibility: hidden;
-	}
 </style>
 
 <ul class="nav">
-	<li>
-		<NavLink name={'Home'}>
-			<div class="logo stack">
-				<img src="/images/sublimate.svg" alt="Sublimate" width="150" />
-				<h1>Sublimate</h1>
-			</div>
-		</NavLink>
-	</li>
 	{#each links as link}
 		<li class="font-bold">
 			<NavLink name={typeof link === 'string' ? link : link.name}>
@@ -86,7 +51,4 @@
 			</NavLink>
 		</li>
 	{/each}
-	<li>
-		<Wallet />
-	</li>
 </ul>
