1029f2492d345d9f0a31a055031305aecabca02e
Darryl Yeo <dev@darryl-yeo.com>
2021-01-28 23:19:44 -0800

    Silence warnings about missing sourcemaps in web3w package

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,6 @@ export default {
 	// minify: false,
 	// sourcemap: true,
 	optimizeDeps: {
-		exclude: ['web3w', 'sublimate-common'], // allow to develop with hot reload
+		exclude: [/*'web3w',*/ 'sublimate-common'], // allow to develop with hot reload
 	},
 }
