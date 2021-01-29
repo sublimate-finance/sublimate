1a793cefe982f4e45b84243f210e2760ab53a7db
Darryl Yeo <dev@darryl-yeo.com>
2021-01-29 02:46:14 -0800

    Add transitions

--- web/src/components/SubscriptionsSummary.svelte
+++ web/src/components/SubscriptionsSummary.svelte
@@ -13,6 +13,7 @@
 	import Select from '../components/Select.svelte'
 	import TokenValue from './TokenValue.svelte'
 
+	import { scale } from 'svelte/transition'
 </script>
 
 <style>
@@ -25,32 +26,38 @@
 	<div class="neumorphic columns">
 		<Select options={['Summary', 'Incoming', 'Supporting']} bind:value={selected} style="full" />
 	</div>
-	<div>
+	<div class="stack">
 		{#if selected === 'Summary'}
-			<div class="columns stats">
-				<div class="column">
-					<span class="boxed neumorphic">≈ <TokenValue value={13,324.04} token="ETH" /></span>
-					<span>collected since</span>
-					<strong>January 2017</strong>.
+			<div class="column" transition:scale={{start: 0.8}}>
+				<div class="columns stats">
+					<div class="column">
+						<span class="boxed neumorphic">≈ <TokenValue value={13,324.04} token="ETH" /></span>
+						<span>collected since</span>
+						<strong>January 2017</strong>.
+					</div>
+					<div class="column">
+						<span class="boxed neumorphic">≈ <TokenValue value={24.04} token="ETH" /></span>
+						<span>collected in the last</span>
+						<strong>24 hours</strong>
+					</div>
+					<div class="column">
+						<span class="boxed neumorphic">145 donators</span>
+						<span>since</span>
+						<strong>January 2017</strong>.
+					</div>
 				</div>
-				<div class="column">
-					<span class="boxed neumorphic">≈ <TokenValue value={24.04} token="ETH" /></span>
-					<span>collected in the last</span>
-					<strong>24 hours</strong>
+				<div class="boxed neumorphic">
+					<Select options={timeIntervals} bind:value={timeInterval} />
 				</div>
-				<div class="column">
-					<span class="boxed neumorphic">145 donators</span>
-					<span>since</span>
-					<strong>January 2017</strong>.
-				</div>
-			</div>
-			<div class="boxed neumorphic">
-				<Select options={timeIntervals} bind:value={timeInterval} />
 			</div>
 		{:else if selected === 'Incoming'}
-			<IncomingSubscriptionsSummary {incomingSubscriptions} />
+			<div class="column" transition:scale={{start: 0.8}}>
+				<IncomingSubscriptionsSummary {incomingSubscriptions} />
+			</div>
 		{:else if selected === 'Supporting'}
-			<OutgoingSubscriptionsSummary {outgoingSubscriptions} />
+			<div class="column" transition:scale={{start: 0.8}}>
+				<OutgoingSubscriptionsSummary {outgoingSubscriptions} />
+			</div>
 		{/if}
 	</div>
 </div>
