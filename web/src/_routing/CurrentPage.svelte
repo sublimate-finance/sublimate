952d50182cbcd251538cc7fac12f1a8983abedac
Darryl Yeo <dev@darryl-yeo.com>
2021-01-28 23:26:18 -0800

    Smooth transition when navigating from page to page!

--- web/src/_routing/CurrentPage.svelte
+++ web/src/_routing/CurrentPage.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import {getResponse} from '@curi/svelte';
 	const response = getResponse();
+
+	import { scale } from 'svelte/transition'
 </script>
 
-{#if $response}
-	<svelte:component this={$response.body} />
-{:else}
-	<slot />
-{/if}
+<main class="stack">
+	{#if $response}
+		{#key $response.body}
+			<div transition:scale={{start: 0.9}}>
+				<svelte:component this={$response.body} />
+			</div>
+		{/key}
+	{:else}
+		<slot />
+	{/if}
+</main>
