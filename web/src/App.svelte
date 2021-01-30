8471b3adb0bddb7e671196337383868b1329010c
Darryl Yeo <dev@darryl-yeo.com>
2021-01-30 15:46:11 -0800

    Fix CSS

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -6,8 +6,9 @@
 <style global>
 	@import "../assets/fonts/fonts.css";
 	@import "../assets/global.css";
-	/* @import "tailwindcss/base.css"; */
-	@import "tailwindcss/components.css";
+
+	/* @import "tailwindcss/base.css";
+	@import "tailwindcss/components.css"; */
 	@import "tailwindcss/utilities.css";
 </style>
 
