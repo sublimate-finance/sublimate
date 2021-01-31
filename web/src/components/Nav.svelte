0a421c32dd88ee658fb39f71ec1248155a590923
Darryl Yeo <dev@darryl-yeo.com>
2021-01-30 18:19:41 -0800

    Move most pages and packages over

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,60 +1,54 @@
 <script lang="ts">
-	export let segment: string;
+	type LinkInfo = string | {name: string; title: string}
+
+	export let links: LinkInfo[]
+
+	import NavLink from './NavLink.svelte'
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
+	.nav {
+		display: flex;
+		align-items: stretch;
 	}
 
 	li {
-		display: block;
-		float: left;
+		display: flex;
+		align-items: center;
+		padding: 0 calc(var(--space-inner) / 2);
+	}
+	li:first-child {
+		margin-right: auto;
 	}
 
-	[aria-current] {
-		position: relative;
-		display: inline-block;
+	li > :global(*) {
+		--space-inner: 1em;
 	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
+	li > :global(a) {
+		--border-width: 2px;
+		display: flex;
+		align-self: stretch;
+		align-items: center;
+		padding: 0 calc(var(--space-inner) / 2);
+		margin: 0 calc(-0.5 * var(--border-width));
+		border-bottom: var(--border-width) solid;
+		border-bottom-color: transparent;
 		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
+	}
+	li > :global(a:hover) {
+		border-bottom-color: currentColor;
+	}
+	li > :global(a.active) {
+		border-bottom-color: var(--accent-color);
 	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
+<ul class="nav">
+	{#each links as link}
+		<li class="font-bold">
+			<NavLink name={typeof link === 'string' ? link : link.name}>
+				{typeof link === 'string' ? link : link.title}
+			</NavLink>
+		</li>
+	{/each}
+</ul>
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,50 +1,64 @@
-<script>
-	import successkid from 'images/successkid.jpg';
+<script lang="ts">
+	import { featuredCreators } from '../stores/creators'
+
+	import Button from '../components/Button.svelte'
+	import CreatorCards from '../components/CreatorCards.svelte'
 </script>
 
 <style>
-	h1, figure, p {
+	section {
+		font-size: 1.5em;
+		padding: 4rem;
+		--space-inner: 2rem;
+		min-height: 70vh;
 		text-align: center;
-		margin: 0 auto;
+		align-content: center;
 	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
+	section.centered {
+		justify-items: center;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
+	section.accented {
+		background: radial-gradient(var(--accent-color-light) 50%, var(--accent-color));
+	}
+	section.accented h2 {
+		color: white;
 	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+	#creators :global(.creator-cards) {
+		font-size: 0.6em;
 	}
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
+<!-- <section id="top">
+	<h2>Reward creativity.<br>Make every second count.</h2>
+	<div>
+		<Button href="explore">Start accepting donations</Button>
+	</div>
 
-<h1>Great success!</h1>
+	<p>Accept continuous financial support from your audience.</p>
+	<img src="images/wallet-illustration.png" />
+</section> -->
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<section id="top" class="centered">
+	<img src="images/wallet-illustration.png" />
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<h2>Make every second count.</h2>
+
+	<p>Accept <b>continuous financial support</b> from your audience, <br>so you can focus on <b>making amazing things</b>.</p>
+
+	<div>
+		<Button href="#creators">Learn more</Button>
+		<Button href="dashboard" class="accented">Start a creator page</Button>
+	</div>
+</section>
+
+<section id="creators" class="accented">
+	<h2>We love our developers and content creators</h2>
+	<CreatorCards creators={featuredCreators} />
+	<div>
+		<Button href="explore">Explore Creators</Button>
+	</div>
+</section>
+
