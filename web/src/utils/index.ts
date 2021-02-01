19964d5c9ccf6de282a1e6ad4468c9b0cef7cecc
ulydev <ulydev@live.fr>
2021-02-01 09:22:35 +0100

    add resolveENS draft

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -3,3 +3,7 @@ export function wait<T>(numSeconds: number, v: T): Promise<T> {
 		setTimeout(resolve.bind(null, v), numSeconds * 1000)
 	})
 }
+
+export const resolveENS = async (ens: string) => {
+	return ens // TODO: resolve ens name to pass in subscribe()
+}
