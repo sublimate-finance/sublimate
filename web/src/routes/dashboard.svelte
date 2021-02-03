a1b1b55d3c01d3a0abdaf8e4845f32438ab8f1fc
Darryl Yeo <dev@darryl-yeo.com>
2021-02-02 21:39:35 -0800

    Create <ProfileEditor> component with initial edit capability

--- web/src/routes/dashboard.svelte
+++ web/src/routes/dashboard.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { TimeInterval, timeIntervals } from '../types/time-intervals'
-
 	import { creators } from '../stores/creators'
 
 	const creator = creators[0]
@@ -9,6 +7,7 @@
 	import Button from '../components/Button.svelte'
 	import CreatorChart from '../components/CreatorChart.svelte'
 	import Footer from '../components/Footer.svelte'
+	import ProfileEditor from '../components/ProfileEditor.svelte'
 	import ProfileSummary from '../components/ProfileSummary.svelte'
 	import SubscriptionsSummary from '../components/SubscriptionsSummary.svelte'
 </script>
@@ -27,16 +26,8 @@
 		--space-inner: 1.5em;
 		align-content: start;
 	}
-	.card .row {
-		--space-inner: 1em;
-	}
 	.card .column {
 		--space-inner: 0.5em;
-	}
-
-	.cover-image {
-		width: 100%;
-		height: 8rem;
 	}
 
 	.creator-link {
@@ -47,51 +38,10 @@
 
 <section class="dashboard">
 	<div class="column">
-		<div class="card">
-			<div class="row">
-				<img src={creator.profile.avatar} alt="Avatar" class="block rounded-full" width="80" height="80" />
-				<div class="column">
-					<div class="bar">
-						<h2>{creator.profile.name}</h2>
-						<img src="/images/edit.svg" alt="edit" class="w-4 h-4" />
-					</div>
-					<div class="bar">
-						<span class="text-primary-200">{creator.profile.summary}</span>
-						<img src="/images/edit.svg" alt="edit" class="w-4 h-4" />
-					</div>
-				</div>
-			</div>
-			<div class="column">
-				<div class="column">
-					<h3>About</h3>
-					<textarea class="boxed neumorphic" bind:value={creator.profile.about} rows="3" />
-				</div>
-			</div>
-			<div class="column">
-				<div class="bar">
-					<h3>Links</h3>
-					<img src="/images/edit.svg" alt="edit" class="w-4 h-4" />
-				</div>
-				<label class="column">
-					<span>Website</span>
-					<input type="text" class="neumorphic" bind:value={creator.profile.website} placeholder="https://sublimate.finance" />
-				</label>
-				<label class="column">
-					<span>Twitter</span>
-					<input type="text" class="neumorphic" bind:value={creator.profile.twitter} placeholder="https://twitter.com/SublimateFinance" />
-				</label>
-			</div>
-			<div class="column">
-				<div class="bar">
-					<h3>Cover image</h3>
-					<img src="/images/edit.svg" alt="edit" class="w-4 h-4" />
-				</div>
-				<img class="cover-image rounded-md shadow-inner" src={creator.profile.cover} alt="Cover Image" height="100" />
-			</div>
-		</div>
+		<ProfileEditor profile={creator.profile} />
 		<div class="card">
 			<div class="column">
-				<h3>Share your Sublimate link</h3>
+				<h3>Share your Sublimate profile</h3>
 				<div class="bar boxed neumorphic">
 					<a href="/creator/{creator.address}" class="creator-link">sublimate.finance/creator/{creator.address}</a>
 					<Button>Copy</Button>
@@ -99,7 +49,7 @@
 				</div>
 			</div>
 			<div class="column">
-				<h3>Embed Sublimate within your website</h3>
+				<h3>Embed Sublimate on your website</h3>
 				<div class="columns">
 					<Button>Copy Embed Code</Button>
 				</div>
