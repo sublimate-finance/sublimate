3b8d17291dc5ca64f92b97b68e39a8f833c5eb76
Darryl Yeo <dev@darryl-yeo.com>
2021-02-05 00:27:54 -0800

    Use StreamableWrappedETH contract; update event handler names; fix IDs; correct AssemblyScript syntax

--- subgraph/mappings/streamable-subscriptions.ts
+++ subgraph/mappings/streamable-subscriptions.ts
@@ -4,39 +4,45 @@ import { ByteArray, crypto, log } from '@graphprotocol/graph-ts'
 import { User, Subscription } from '../generated/schema'
 
 // Events
-import { SubscriptionStarted, SubscriptionStopped, SubscriptionCanceled } from '../generated/StreamableERC20/StreamableERC20Contract'
+import { SubscriptionStarted, SubscriptionUpdated, SubscriptionCanceled } from '../generated/StreamableWrappedETH/StreamableWrappedETHContract'
 
 
 export function handleSubscriptionStarted(event: SubscriptionStarted): void {
-	const from = event.params['from'].toHex()
-	const to = event.params['to'].toHex()
-	const rate = event.params['rate']
-	const maxAmount = event.params['maxAmount']
+	// Event params
+	let from = event.params.from.toHex()
+	let to = event.params.to.toHex()
+	let rate = event.params.rate
+	let maxAmount = event.params.maxAmount
+
+
+	// Block info
+	let startBlock = event.block.number
+	let startTime = event.block.timestamp
 
 
 	// Subscription
 
-	// Subscription ID is the hash of (`from` concatenated to `to`)
-	const subscriptionID = crypto.keccak256(ByteArray.fromHexString(from.concat(to)))
-	const subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
+	// Subscription ID = from + to + startBlock
+	let subscriptionID = `${from}-${to}-${startBlock}`
+	let subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
 
 	subscriptionEntity.from = from
 	subscriptionEntity.to = to
 	subscriptionEntity.rate = rate
 	subscriptionEntity.maxAmount = maxAmount
 
-	subscriptionEntity.startBlock = event.block.number
-	subscriptionEntity.startTime = event.block.timestamp
+	subscriptionEntity.startBlock = startBlock
+	subscriptionEntity.startTime = startTime
 	// subscriptionEntity.endBlock =
-	// subscriptionEntity.endTime = 
+	// subscriptionEntity.endTime =
 
 	subscriptionEntity.save()
 
 
 	// `from` User
 
-	const fromUserID = from
-	const fromUserEntity = User.load(fromUserID) || new User(fromUserID)
+	let fromUserID = from
+	let fromUserEntity = User.load(fromUserID) || new User(fromUserID)
 
 	// fromUserID.balance =
 	// ...
@@ -46,26 +52,32 @@ export function handleSubscriptionStarted(event: SubscriptionStarted): void {
 
 	// `to` User
 
-	const toUserID = to
-	const toUserEntity = User.load(toUserID) || new User(toUserID)
+	let toUserID = to
+	let toUserEntity = User.load(toUserID) || new User(toUserID)
 
 	// toUserID.balance =
 	// ...
-	
+
 	toUserEntity.save()
 }
 
 
-export function handleSubscriptionStopped(event: SubscriptionStopped): void {
-	const from = event.params['from'].toHex()
-	const to = event.params['to'].toHex()
+export function handleSubscriptionUpdated(event: SubscriptionUpdated): void {
+	// Event params
+	let from = event.params.from.toHex()
+	let to = event.params.to.toHex()
+
+
+	// Block info
+	let startBlock = event.block.number
+	let startTime = event.block.timestamp
 
 
 	// Subscription
 
-	// Subscription ID is the hash of (`from` concatenated to `to`)
-	const subscriptionID = crypto.keccak256(ByteArray.fromHexString(from.concat(to)))
-	const subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
+	// Subscription ID = from + to + startBlock
+	let subscriptionID = `${from}-${to}-${startBlock}`
+	let subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
 
 	// ...
 
@@ -74,8 +86,8 @@ export function handleSubscriptionStopped(event: SubscriptionStopped): void {
 
 	// `from` User
 
-	const fromUserID = from
-	const fromUserEntity = User.load(fromUserID) || new User(fromUserID)
+	let fromUserID = from
+	let fromUserEntity = User.load(fromUserID) || new User(fromUserID)
 
 	// ...
 
@@ -84,25 +96,31 @@ export function handleSubscriptionStopped(event: SubscriptionStopped): void {
 
 	// `to` User
 
-	const toUserID = to
-	const toUserEntity = User.load(toUserID) || new User(toUserID)
+	let toUserID = to
+	let toUserEntity = User.load(toUserID) || new User(toUserID)
 
 	// ...
-	
+
 	toUserEntity.save()
 }
 
 
 export function handleSubscriptionCanceled(event: SubscriptionCanceled): void {
-	const from = event.params['from'].toHex()
-	const to = event.params['to'].toHex()
+	// Event params
+	let from = event.params.from.toHex()
+	let to = event.params.to.toHex()
+
+
+	// Block info
+	let startBlock = event.block.number
+	let startTime = event.block.timestamp
 
 
 	// Subscription
 
-	// Subscription ID is the hash of (`from` concatenated to `to`)
-	const subscriptionID = crypto.keccak256(ByteArray.fromHexString(from.concat(to)))
-	const subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
+	// Subscription ID = from + to + startBlock
+	let subscriptionID = `${from}-${to}-${startBlock}`
+	let subscriptionEntity = Subscription.load(subscriptionID) || new Subscription(subscriptionID)
 
 	// ...
 
@@ -111,8 +129,8 @@ export function handleSubscriptionCanceled(event: SubscriptionCanceled): void {
 
 	// `from` User
 
-	const fromUserID = from
-	const fromUserEntity = User.load(fromUserID) || new User(fromUserID)
+	let fromUserID = from
+	let fromUserEntity = User.load(fromUserID) || new User(fromUserID)
 
 	// ...
 
@@ -121,10 +139,10 @@ export function handleSubscriptionCanceled(event: SubscriptionCanceled): void {
 
 	// `to` User
 
-	const toUserID = to
-	const toUserEntity = User.load(toUserID) || new User(toUserID)
+	let toUserID = to
+	let toUserEntity = User.load(toUserID) || new User(toUserID)
 
 	// ...
-	
+
 	toUserEntity.save()
 }
